ed62d8000f68ea1d81bb0a8a7472d0c557b14494
mac15001900 <mac15001900@gmail.com>
2016-03-12 15:24:04 +0000

    Added working button

--- minigame/minigame.js
+++ minigame/minigame.js
@@ -10,23 +10,89 @@ var context = canvas.getContext('2d');
 
 var delta = 0;
 var now = Date.now();
+var button = new Button(400,300,100);
 
 
 function init(){
-    
+    canvas.addEventListener("mousedown", onMouseDown, false);
 }
 
 var render = function(){
-    
-}
+    button.render();
+};
 
 var update = function(){
     
-}
+};
 
 var step = function(){
     update();
     render();
     animate(step);
+};
+
+function Button(x,y,size){
+    this.x = x;
+    this.y = y;
+    this.size= size;
+    this.clicks = 0;
+    this.render = function(){
+        context.beginPath();
+        context.arc(x,y,size/2,0,Math.PI*2,false);
+        context.fillStyle = "#0000FF";
+        context.fill();
+    }
+    this.tryClick = function(mouse){
+        var distance = Math.sqrt(Math.pow(x-mouse.x,2) + Math.pow(y-mouse.y,2));
+        if(distance<=size/2) this.newClick();
+    }
+    this.newClick = function(){
+        this.clicks++;
+        $("#log1").text(this.clicks);
+    }
+    
+    
+    
 }
 
+
+
+
+
+
+
+function onMouseDown(e) {
+    $("#log1").text("Registered click");
+  var mouse = getMouse(e, canvas);
+  button.tryClick(mouse);
+   $("#debug2").text("Mouse click at"+mouse.x);
+}
+
+
+
+//getMouse() From http://stackoverflow.com/questions/10449890/detect-mouse-click-location-within-canvas#10450915
+function getMouse(e, canvas) {
+  var element = canvas, offsetX = 0, offsetY = 0, mx, my;
+
+  // Compute the total offset. It's possible to cache this if you want
+  if (element.offsetParent !== undefined) {
+    do {
+      offsetX += element.offsetLeft;
+      offsetY += element.offsetTop;
+    } while ((element = element.offsetParent));
+  }
+
+  //Could potentially do that later
+  //offsetX += stylePaddingLeft + styleBorderLeft + htmlLeft;
+  //offsetY += stylePaddingTop + styleBorderTop + htmlTop;
+
+  mx = e.pageX - offsetX;
+  my = e.pageY - offsetY;
+
+  // We return a simple javascript object with x and y defined
+  return {x: mx, y: my};
+}
+
+init();
+document.body.appendChild(canvas);
+animate(step);
