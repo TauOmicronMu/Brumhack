56594a3c19c4ef893a2e2641b1569419d239d82b
TauOmicronMu <TauOmicronMuRS@Gmail.com>
2016-03-12 15:50:22 +0000

    More work :3

---
+++ src/spinpage.js
@@ -0,0 +1,11 @@
+var wheel = new Winwheel({
+        'numSegments' : 4,
+        'segments'    :
+            [
+                {'fillStyle' : '#eae56f', 'text' : 'Tom'},
+                {'fillStyle' : '#89f26e', 'text' : 'Mac'},
+                {'fillStyle' : '#7de6ef', 'text' : 'Matt'},
+                {'fillStyle' : '#e7706f', 'text' : 'Liam'}
+            ]
+});
+
