52effb25554a137505d3fbf72e698208a94ad06d
TauOmicronMu <TauOmicronMuRS@Gmail.com>
2016-03-13 01:38:48 +0000

    Added ALL the shit.

--- src/app.js
+++ src/app.js
@@ -1,12 +1,25 @@
-function displayPageOne {
-};
+var pages = ["#page1", "#page2", "#page3", "#page4"];
 
-function displayPageTwo {
-};
-
-function displayPageThree {
-};
-
-function displayPageFour {
-};
+function moveIntoView(n) {
+    /*
+     * Move the page into view.
+     */
+    console.log("Attempted to move page " + n + " into view.");
+    $(pages[n-1]).css("left","0px");
+    $(pages[n-1]).css("up","0px");
+    $(pages[n-1]).css("height","100%");
+    $(pages[n-1]).css("width","100%");
+    /*
+     * Move the other pages out of view.
+     */
+     for(var i = 0; i < pages.length; i++) {
+         if(!((n-1) === i)) {
+             console.log("Attempted to move page " + i + " into view.");
+             $(pages[i]).css("left","-500%");
+             $(pages[i]).css("up","-500%");
+             $(pages[i]).css("height","0px");
+             $(pages[i]).css("width","0px");
+         }
+     }
+}; 
 
